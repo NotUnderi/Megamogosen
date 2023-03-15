d76bdfe5746f8c1061ce323a29a84b25d3e25aeb
Underi <undershotbutton@gmail.com>
2023-03-15 13:50:00 +0200

    Revert "Changed enemy death handling"
    This reverts commit 13f45ef912b381db6b7f42bb8d0e2f5c5a464d4e.

--- yes/game.py
+++ yes/game.py
@@ -45,7 +45,7 @@ def draw_game():
     win.fill((0,0,0))
     win.blit(bg, (0, 0))
     player.draw(win)
-    pygame.draw.rect(win,(0,255,0),player.rect)
+    #pygame.draw.rect(win,(0,255,0),player.rect)
     for e in enemies:
         e.draw(win)
         #pygame.draw.rect(win,(255,0,0),e.rect)            #comment draw.rects out, only for debugging hitboxes
@@ -242,8 +242,6 @@ class Enemy:
 
     def death(self): 
         pygame.mixer.Sound.play(death_sound)
-        self.y = 10000
-        self.x = -50000             #questionable practice
         player.ammo += 2
 
 
